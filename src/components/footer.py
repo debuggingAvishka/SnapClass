4dac18e52bbff09be765103a7afd7fb71b7bdfbe
Avishka Mittal <avishkamittal90@gmail.com>
2026-06-12 12:15:43 +0530

    streamlit setup + homescreen

---
+++ src/components/footer.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+def footer_home():
+    
+    logo_url = "https://i.ibb.co/4r5X1FY/apnacollege.png"
+    
+    st.markdown(f"""
+                <div style="margin-top:2rem; diplay:flex; gap:6px; justify-content:center; items-align:center;">
+                <p style="font-weight:bold; color:white;" > Created with dedication by 
+                <img src='{logo_url}' style='max-height:25px;'/> </p>
+                </div>
+                """, unsafe_allow_html=True)
---
+++ src/ui/base_layout.py
@@ -0,0 +1,101 @@
+import streamlit as st
+
+def style_base_layout():
+    st.markdown("""
+        <style>     
+            @import url('https://fonts.googleapis.com/css2?family=Climate+Crisis:YEAR@1979&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+            @import url('https://fonts.googleapis.com/css2?family=Climate+Crisis:YEAR@1979&family=Outfit:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+        /* Hide top of streamlit */
+                #MainMenu, footer, header {
+                    visibility: hidden;
+                }
+
+                .block-container {
+                    padding-top:1.5rem !important;
+                }
+                
+                h1{
+                    font-family: 'Climate Crisis', sans-serif !important;
+                    font-size: 3.5rem !important;
+                    line-height: 1.1 !important;
+                    margin-bottom:0rem !important;
+                }
+                
+                h2{
+                    font-family: 'Climate Crisis', sans-serif !important;
+                    font-size: 2rem !important;
+                    line-height: 0.9 !important;
+                    margin-bottom:0rem !important;
+                }
+                
+                h3,h4,p{
+                    font-family: 'Outfit', sans-serif;
+                }
+                
+                button{
+                    border-radius: 1.5rem !important;
+                    background: #5865F2 !important;
+                    color: white !important;
+                    padding: 10px 20px !important;
+                    border:none !important;
+                    transition: transform 0.25s ease-in-out !important;
+                }
+                
+                button[kind="secondary"]{
+                    border-radius: 1.5rem !important;
+                    background: #EB459E !important;
+                    color: white !important;
+                    padding: 10px 20px !important;
+                    border:none !important;
+                    transition: transform 0.25s ease-in-out !important;
+                }
+                button[kind="tertiary"]{
+                    border-radius: 1.5rem !important;
+                    background: black !important;
+                    color: white !important;
+                    padding: 10px 20px !important;
+                    border:none !important;
+                    transition: transform 0.25s ease-in-out !important;
+                }
+                
+                button:hover{
+                    transform : scale(1.05)
+                }
+        </style>
+
+                """,
+                unsafe_allow_html=True)
+    
+
+def style_background_home():
+    st.markdown("""
+        <style>
+
+                .stApp {
+                    background: #5865F2 !important
+                } 
+                
+                .stApp div[data-testid="stColumn"]{
+                    background-color:#E0E3FF !important;
+                    padding:2.5rem !important;
+                    border-radius: 5rem !important;
+                } 
+
+        </style>
+
+                """,
+                unsafe_allow_html=True)
+    
+def style_background_dashboard():
+    st.markdown("""
+        <style>
+
+                .stApp {
+                    background: #E0E3FF !important
+                }  
+
+        </style>
+
+                """,
+                unsafe_allow_html=True)
